183f0a1f703e278713df95fcf6e5e6872042a3b6
chris.hmc <hsiao516@gmail.com>
2020-12-01 17:53:22 +0800

    [新增] 第一版todo list完成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todo, setTodo] = useState('');
+    const [todoList, setTodoList] = useState([]);
+
+    function handleUpdateText(e) {
+        setTodo(e.target.value);
+    }
+
+    function handleAdd() {
+        setTodoList([...todoList, todo]);
+        setTodo('');
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <input type="text" value={todo} onChange={handleUpdateText}/>
+                <button onClick={handleAdd}>新增</button>
+            </div>
+            {
+                todoList.map(todo => {
+                    return <div key={todo}>{todo}</div>
+                })
+            }
+        </div>
+    );
 }
 
 export default App;
